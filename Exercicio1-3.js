65443750d07ce958cfe219b05d886d29d718b74f
gustavoWuerges <gugu.wuerges@gmail.com>
2025-09-17 21:27:32 -0300

    Primeiro Commit Main

--- Exercicio1-3.js
+++ Exercicio1-3.js
@@ -1,12 +1,11 @@
 // Complete a função que verifica se um número é par ou ímpar
 function verificarParImpar(numero) {
-    if (numero % 2 == 0) {
+    if (numero % 2 === 0) {
         return "par";
     } else {
         return "ímpar";
     }
 }
 
-// Teste a função
-console.log(verificarParImpar(8));  // deve retornar "par"
-console.log(verificarParImpar(7));  // deve retornar "ímpar"
+console.log(verificarParImpar(8)); 
+console.log(verificarParImpar(7));  
---
+++ Exercicio2-1.js
@@ -0,0 +1,9 @@
+
+let frutas = ["Maçã", "Banana", "Laranja", "Uva", "Manga"];
+
+
+console.log("Primeira fruta:", frutas[0]);
+console.log("Última fruta:", frutas[frutas.length - 1]);
+
+
+console.log("Total de frutas:", frutas.length);
