7fe253a0b740a6f928b3e0ce1310bd71051b6d86
gustavoWuerges <gugu.wuerges@gmail.com>
2025-09-17 21:21:10 -0300

    Primeiro Commit Main

---
+++ Exercicio1-1.js
@@ -0,0 +1,5 @@
+let meuNome = "Gustavo Wuerges"
+let minhaIdade = "18"
+let minhaCidade = "Pomerode"
+
+console.log("Meu nome: " + meuNome + "\nMinha idade: " + minhaIdade + "\nMinha Cidade: " + minhaCidade);
